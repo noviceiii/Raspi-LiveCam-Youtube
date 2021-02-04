5a5a1824cd9fd8877541d54d5f51f746820b6019
Oliver <nospam1@meister.cx>
2021-02-04 21:56:45 +0100

    Update streamer.sh
    Some tweaks to enhance performance
    - mp3 without encoding
    - output frame rate
    - output buffer size
    - maxrate for output bitrate with buffer size

--- streamer.sh
+++ streamer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #   Raspii Live Stream to youtube with music.
-#   Version 2.1, February 4th, 2021 by Oliver.
+#   Version 2.3, February 4th, 2021 by Oliver.
 
 #   Drawtext makes it a little slow. Delete drawtext for better performance.
+#   Bitrate shouldn't drop below 400 kbit/s; speed shouldn't be less than 1x
+
 #   Change below values according your needs.
 
 yttoken="your-token-here"                   # string. Youtube token fore live videos.
@@ -15,16 +17,14 @@ b=6000000                                   # int. Bit rate for input vidoe from
 FPATH="/etc/fonts/FONTFILE.ttf"             # STRING. full path to font file. Optain from google fonts.
 OPATH="/path/to/overlay.txt"                # STRING. full path to text file with the text to be embeded.
 
-or=15                                       # int. Bit rate to create output stream. 
-                                            # you can set it higher but drawtext makes it all slow.
-                                            # and speed shouldn't drop below 1.0
+or=15                                       # int. frame rate to create output stream. 
 
 # -----------------------------------------------------------------------------------------------------
 
 raspivid -o - -t 0 -vf -hf -fps $ir -b $b | \
 ffmpeg -stream_loop -1 -safe 0 -i $pll \
--f h264 -i - -acodec aac -ab 128k -g 50 -strict experimental \
+-f h264 -i - -acodec copy -strict experimental \
 -vf drawtext="fontfile=$FPATH: \
     textfile=$OPATH: fontcolor=white: fontsize=56: box=1: boxcolor=black@0.5: \
     boxborderw=5: x=w-tw-10: y=h-th-10: reload=1" \
--r $or -f flv rtmp://a.rtmp.youtube.com/live2/$yttoken
+-r $or -maxrate 2000k -bufsize 10M -f flv rtmp://a.rtmp.youtube.com/live2/$yttoken
